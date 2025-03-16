72e7aa2fd6db5e7c12e5c425b1cd2e6b01253356
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-03-16 19:35:36 +0000

    css and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mr. Coxall updated by Annasophia
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+
+function myButtonClicked () {
+  alert('Hello, World!')
+}
